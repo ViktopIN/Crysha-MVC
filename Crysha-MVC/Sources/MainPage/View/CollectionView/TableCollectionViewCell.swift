37ecfcdaf6a1620d646f399764f98a8c11add5c5
Viktor <viktopiniaginya@gmail.com>
2022-10-26 17:56:08 +0300

    add top collectionview

--- Crysha-MVC/Sources/MainPage/View/CollectionView/TableCollectionViewCell.swift
+++ Crysha-MVC/Sources/MainPage/View/CollectionView/TableCollectionViewCell.swift
@@ -11,12 +11,13 @@ class TableCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Properties
     
-    let reuseID = "tableCollectionViewCell"
+    static let reuseID = "tableCollectionViewCell"
     
     // MARK: - Views
     private lazy var icon: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.contentsGravity = .resizeAspectFill
         return imageView
     }()
     
@@ -35,25 +36,95 @@ class TableCollectionViewCell: UICollectionViewCell {
     
     private lazy var countInSection: UILabel = {
         let label = UILabel()
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 16)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    private lazy var disclosureIndicatorImage: UIImageView = {
+        let image = UIImage(systemName: "chevron.right")?.withTintColor(UIColor.gray, renderingMode: .alwaysOriginal)
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     // MARK: - Initialisation
     
-    init() {
-        super.init(frame: .zero)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         hierarchySetup()
         layoutSetup()
+        getBottomLayer()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Settings
+    
+    private func hierarchySetup() {
+        contentView.addSubview(icon)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(disclosureIndicatorImage)
+    }
+    
+    private func layoutSetup() {
+        NSLayoutConstraint.activate(
+            [
+                icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 28),
+                icon.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 4/11),
+                icon.widthAnchor.constraint(equalTo: icon.heightAnchor)
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                nameLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 15),
+                nameLabel.heightAnchor.constraint(equalTo: icon.heightAnchor)
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                disclosureIndicatorImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                disclosureIndicatorImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+                disclosureIndicatorImage.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 3/11),
+                disclosureIndicatorImage.widthAnchor.constraint(equalTo: disclosureIndicatorImage.heightAnchor, multiplier: 1/2)
+            ]
+        )
+    }
+    
+    private func addNewImageSectioin() {
+        self.contentView.addSubview(self.newImageSection)
+        NSLayoutConstraint.activate(
+            [
+                self.newImageSection.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+                self.newImageSection.leadingAnchor.constraint(equalTo: self.nameLabel.trailingAnchor, constant: 13),
+                self.newImageSection.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 4/5),
+                self.newImageSection.widthAnchor.constraint(equalTo: newImageSection.heightAnchor)
+            ]
+        )
+    }
+    
+    private func addCountInSection() {
+        self.contentView.addSubview(self.countInSection)
+        NSLayoutConstraint.activate(
+            [
+                self.countInSection.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+                self.countInSection.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -50),
+                self.countInSection.heightAnchor.constraint(equalTo: self.icon.heightAnchor)
+            ]
+        )
+    }
+    
     // MARK: - Methods
     
     func getModelToCell(this model: TableCellModel) {
-        icon.image = model.image
+        icon.image = model.image.withTintColor(.gray, renderingMode: .alwaysOriginal)
         nameLabel.text = model.name
         if model.isNewSection {
             addNewImageSectioin()
@@ -64,54 +135,18 @@ class TableCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    private func hierarchySetup() {
-        contentView.addSubview(icon)
-        contentView.addSubview(nameLabel)
-    }
-    
-    private func layoutSetup() {
-        NSLayoutConstraint.activate(
-            [
-                icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-                icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-                icon.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 1/3),
-                icon.widthAnchor.constraint(equalTo: icon.heightAnchor)
-            ]
-        )
+    private func getBottomLayer() {
+        let bottomLineLayer = CAShapeLayer()
+        layer.addSublayer(bottomLineLayer)
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 28 + 24 + 7.5, y: contentView.bounds.maxY))
+        path.addLine(to: CGPoint(x: contentView.bounds.maxX, y: contentView.bounds.maxY))
+        bottomLineLayer.path = path.cgPath
+        bottomLineLayer.strokeColor = UIColor.lightGray.cgColor
+        bottomLineLayer.lineWidth = 0.5
+        bottomLineLayer.strokeEnd = 1
+        bottomLineLayer.fillColor = nil
+        bottomLineLayer.lineCap = .round
         
-        NSLayoutConstraint.activate(
-            [
-                nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-                nameLabel.leadingAnchor.constraint(equalTo: icon.leadingAnchor, constant: 10),
-                nameLabel.heightAnchor.constraint(equalTo: icon.heightAnchor)
-            ]
-        )
-    }
-    
-    private func addNewImageSectioin() {
-        DispatchQueue.main.async {
-            self.contentView.addSubview(self.newImageSection)
-            NSLayoutConstraint.activate(
-                [
-                    self.newImageSection.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
-                    self.newImageSection.leadingAnchor.constraint(equalTo: self.nameLabel.leadingAnchor, constant: 10),
-                    self.newImageSection.heightAnchor.constraint(equalTo: self.icon.heightAnchor),
-                    self.newImageSection.widthAnchor.constraint(equalTo: self.icon.heightAnchor)
-                ]
-            )
-        }
-    }
-    
-    private func addCountInSection() {
-        DispatchQueue.main.async {
-            self.contentView.addSubview(self.newImageSection)
-            NSLayoutConstraint.activate(
-                [
-                    self.countInSection.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
-                    self.countInSection.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: 30),
-                    self.countInSection.heightAnchor.constraint(equalTo: self.icon.heightAnchor)
-                ]
-            )
-        }
     }
 }
--- Crysha-MVC/Sources/MainPage/View/MainPage.swift
+++ Crysha-MVC/Sources/MainPage/View/MainPage.swift
@@ -10,11 +10,107 @@ import UIKit
 class MainPageViewController: UIViewController {
     
     // MARK: - Views -
-    let collectionView: UICollectionView! = nil
     
-    // MARK: - Initialisation
+    var collectionView: UICollectionView! = nil
+    
+    // MARK: - Initialisation -
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Settings -
+    
+    private func configure() {
+        let collectionView = UICollectionView(frame: view.frame,
+                                              collectionViewLayout: getLayout())
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(TableCollectionViewCell.self,
+                                 forCellWithReuseIdentifier: TableCollectionViewCell.reuseID)
+        self.collectionView = collectionView
+    }
+    
+    private func setupHierarchy() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate(
+            [
+                collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+                collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ]
+        )
+    }
+    
+    // MARK: - Methods -
+    
+    private func getLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { indexSection, environment ->
+            NSCollectionLayoutSection? in
+            if indexSection == 0 {
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                      heightDimension: .fractionalHeight(1/7))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                       heightDimension: .fractionalHeight(3/7))
+
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+
+                let section = NSCollectionLayoutSection(group: group)
+                return section
+            }
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                  heightDimension: .fractionalHeight(1/7))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .fractionalHeight(3/8))
+
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+        return layout
+    }
+}
+
+// MARK: - Extension Datasource
+
+extension MainPageViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TableCollectionViewCell.reuseID,
+                                                          for: indexPath) as! TableCollectionViewCell
+            cell.getModelToCell(this: TableCellModel.getModels()[indexPath.row])
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TableCollectionViewCell.reuseID,
+                                                      for: indexPath) as! TableCollectionViewCell
+        cell.getModelToCell(this: TableCellModel.getModels()[indexPath.section])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return TableCellModel.getModels().count
+        }
+        return 0
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
     }
 }
 
